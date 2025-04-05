38ae454754970e96d462c8190ca8bf722f35d882
Gizmo207 <investing2188@gmail.com>
2025-04-05 19:42:40 -0400

    📌 Placeholder added: Change Email screen scaffolded for future settings

---
+++ src/app/auth/change-email/page.tsx
@@ -0,0 +1,8 @@
+// TODO: Change Email UI will go here when user settings are implemented
+
+
+
+export default function ChangeEmailPage() {
+    return <div className="text-white p-8">Change Email screen coming soon...</div>;
+  }
+  
